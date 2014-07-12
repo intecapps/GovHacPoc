26685c9948aa6dbfd00e4b42c6029c1509c99254
George Halteh <george.halteh@intecgroup.com.au>
2014-07-12 11:43:40 +0930

    heritage api checkin

---
+++ WebAPI/Heritage/Heritage/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Heritage.Models
+{
+    public class Image
+    {
+        public long ImageId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long AcreID { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ WebAPI/Heritage/Heritage/Models/Level.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Heritage.Models
+{
+    public class Level
+    {
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
